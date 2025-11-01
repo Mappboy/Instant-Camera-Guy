415b2f7c60608d9412cb3524b4082dfa68c5cbb3
Cam <Cameron.j.poole@gmail.com>
2025-11-02 10:25:31 +1100

    feat: add PWA support and configure GitHub Pages deployment workflow
    - Updated vite.config.ts to include VitePWA plugin and set base path for deployment.
    - Created a new GitHub Actions workflow for deploying static content to GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,19 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'; 
+import { VitePWA } from 'vite-plugin-pwa'; 
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Intstant-Camera-Guy-2025-Redesign/',
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react(),
-        tailwindcss()
+        tailwindcss(),
+            VitePWA()
       ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -20,11 +23,7 @@ export default defineConfig(({ mode }) => {
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
-          'buffer': 'buffer/',
         }
       },
-      optimizeDeps: {
-        include: ['buffer'],
-      }
     };
 });
